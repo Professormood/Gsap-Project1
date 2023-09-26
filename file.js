1503b15decb38bae9ffc9808cb397ae07b828d94
Professormood <92402981+Professormood@users.noreply.github.com>
2023-09-27 03:00:44 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,20 @@
+gsap.to("#image1",{
+    x:40,
+    y:50,
+    duration:2,
+    delay:1
+})
+
+gsap.from("#image2",{
+    x:70,
+    y:50,
+    duration:2,
+    delay:1
+})
+
+gsap.from("#header-image",{
+    y:-100,
+    duration:1.5,
+    delay:1,
+    opacity:0
+})
